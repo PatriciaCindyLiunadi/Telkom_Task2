2d052760f3cbcf02287b8b47e1d36fad7517a955
PatriciaCindyLiunadi <patricia.liunadi@binus.ac.id>
2023-04-04 21:12:15 +0700

    Create 5.js

---
+++ 5.js
@@ -0,0 +1,24 @@
+class Car {
+    constructor(brand, year, country) {
+      this.brand = brand;
+      this.year = year;
+      this.country = country;
+    }
+  
+    getAge() {
+      let currentYear = new Date().getFullYear();
+      return currentYear - this.year;
+    }
+  
+    isMadeInUS() {[]
+      return this.country === "US";
+    }
+  }
+
+let myCar = new Car("Toyota", 2015, "Japan");
+
+console.log(myCar.brand);
+console.log(myCar.year);
+console.log(myCar.country);
+console.log(myCar.getAge());
+console.log(myCar.isMadeInUS());
